02a2106b19224a6d07c11b2e4d73c9e8a3827701
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2024-10-13 12:00:36 +0800

    Landing page with Log In and Sign Up Form

---
+++ landing-page/landing-page.js
@@ -0,0 +1,101 @@
+// displaying the dropdown
+function logIn() {
+  document.getElementById("login-dropdown").classList.toggle("log-in-show");
+}
+function signUp() {
+  document.getElementById("signup-dropdown").classList.toggle("sign-up-show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".log-in-drop-btn")) {
+    var loginDropdown = document.getElementById("login-dropdown");
+    if (loginDropdown.classList.contains("log-in-show")) {
+      loginDropdown.classList.remove("log-in-show");
+    }
+  }
+
+  if (!e.target.matches(".sign-up-drop-btn")) {
+    var signupDropdown = document.getElementById("signup-dropdown");
+    if (signupDropdown.classList.contains("sign-up-show")) {
+      signupDropdown.classList.remove("sign-up-show");
+    }
+  }
+};
+
+// displaying customer sign up form
+function displayCustomerSignUp() {
+  const cusSignUpForm = document.getElementById("customer-sign-up");
+
+  cusSignUpForm.style.display = "block";
+
+  setTimeout(() => {
+    document.addEventListener("click", hideFormOnClickOutside);
+  }, 0);
+}
+
+// displaying admin sign up form
+function displayAdminSignUp() {
+  const cusSignUpForm = document.getElementById("admin-sign-up");
+
+  cusSignUpForm.style.display = "block";
+
+  setTimeout(() => {
+    document.addEventListener("click", hideFormOnClickOutside);
+  }, 0);
+}
+
+// displaying customer log in form
+function displayCustomerLogIn() {
+  const cusLogInForm = document.getElementById("customer-log-in");
+
+  cusLogInForm.style.display = "block";
+
+  setTimeout(() => {
+    document.addEventListener("click", hideFormOnClickOutside);
+  }, 0);
+}
+
+// displaying admin log in form
+function displayAdminLogIn() {
+  const adminLogInForm = document.getElementById("customer-log-in");
+
+  adminLogInForm.style.display = "block";
+
+  setTimeout(() => {
+    document.addEventListener("click", hideFormOnClickOutside);
+  }, 0);
+}
+
+// hiding customer or admin sign up/log in form
+function hideFormOnClickOutside(event) {
+  const cusSignUpForm = document.getElementById("customer-sign-up");
+  const adminSignUpForm = document.getElementById("admin-sign-up");
+  const triggerButton = document.getElementsByClassName(".sign-up-button");
+  const cusLogInForm = document.getElementById("customer-log-in");
+  const adminLogInForm = document.getElementById("admin-log-in");
+  const triggerButton2 = document.getElementsByClassName(".log-in-button");
+
+  if (!cusSignUpForm.contains(event.target) && event.target !== triggerButton) {
+    cusSignUpForm.style.display = "none";
+    document.removeEventListener("click", hideFormOnClickOutside);
+  }
+  if (
+    !adminSignUpForm.contains(event.target) &&
+    event.target !== triggerButton
+  ) {
+    adminSignUpForm.style.display = "none";
+    document.removeEventListener("click", hideFormOnClickOutside);
+  }
+  if (
+    !adminLogInForm.contains(event.target) &&
+    event.target !== triggerButton2
+  ) {
+    adminLogInForm.style.display = "none";
+    document.removeEventListener("click", hideFormOnClickOutside);
+  }
+  if (!cusLogInForm.contains(event.target) && event.target !== triggerButton2) {
+    cusLogInForm.style.display = "none";
+    document.removeEventListener("click", hideFormOnClickOutside);
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,18 @@
+FONT:
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+
+font-family: "Playfair Display", serif;
+
+
+COLOR PALETTES:
+#FFEAC5 - light beige
+#FFDBB5 - beige
+#6C4E31 - brown
+#603F26 - dark brown
+
+rgb(255, 234, 197)
+rgb(255, 219, 181)
+rgb(108, 78, 49)
+rgb(96, 63, 38)
