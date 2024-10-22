951192bd7b38415b643a782eb72f39f55144adc7
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2024-10-22 19:09:02 +0800

    added the admin panel and fix the order status

---
+++ admin_panel/controller/addCatController.php
@@ -0,0 +1,18 @@
+<?php
+include_once "../config/dbconnect.php";
+
+if (isset($_POST['upload'])) {
+
+    $catname = $_POST['c_name'];
+
+    $insert = mysqli_query($conn, "INSERT INTO category
+         (category_name) 
+         VALUES ('$catname')");
+
+    if (!$insert) {
+        echo mysqli_error($conn);
+        header("Location: ../sidebar.php?category=error");
+    } else {
+        header("Location: ../admin.php?category=success");
+    }
+}
---
+++ admin_panel/controller/addSizeController.php
@@ -0,0 +1,18 @@
+<?php
+include_once "../config/dbconnect.php";
+
+if (isset($_POST['upload'])) {
+
+    $size = $_POST['size'];
+
+    $insert = mysqli_query($conn, "INSERT INTO sizes
+         (size_name)   VALUES ('$size')");
+
+    if (!$insert) {
+        echo mysqli_error($conn);
+        header("Location: ../admin.php?size=error");
+    } else {
+        echo "Records added successfully.";
+        header("Location: ../admin.php?size=success");
+    }
+}
