ea3dababea45cf28e963f82fbc7c17bfeac4b5b9
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2024-11-06 21:15:57 +0800

    delete some files

--- admin_panel/adminView/viewAdmin.php
+++ admin_panel/adminView/viewAdmin.php
@@ -4,7 +4,6 @@
         <table class="table ">
             <thead>
                 <tr>
-                    <th class="text-center">A.N.</th>
                     <th class="text-center">First Name</th>
                     <th class="text-center">Last Name</th>
                     <th class="text-center">Email</th>
@@ -16,12 +15,10 @@
             include_once "../config/dbconnect.php";
             $sql = "SELECT * from users where users.isAdmin = 1";
             $result = $conn->query($sql);
-            $count = 1;
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
             ?>
                     <tr>
-                        <td><?= $count ?></td>
                         <td><?= $row["first_name"] ?></td>
                         <td><?= $row["last_name"] ?></td>
                         <td><?= $row["email"] ?></td>
@@ -29,7 +26,6 @@
                         <td><button class="btn btn-primary" style="height:40px" onclick="adminEditForm('<?= $row['user_id'] ?>')">Edit</button></td>
                     </tr>
             <?php
-                    $count = $count + 1;
                 }
             }
             ?>
