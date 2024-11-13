c78d0779261198c5db371812ca9707b5230b1c54
pips1804 <jhonpaulmanlapaz1804@gmail.com>
2024-11-13 16:35:53 +0800

    admin is now required to log in first

--- admin_panel/admin.php
+++ admin_panel/admin.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['email'])) {
+
+    header("Location: ../landing-page/index.php?login=required");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
